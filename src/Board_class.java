2532ed7b1fe93d89b336e69adf10baa9cb972055
Josh Budzynski <josh.budzynski@slu.edu>
2024-04-22 14:39:18 -0500

    first commit

---
+++ src/Board_class.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board_class extends JFrame{
+	
+	
+	public Board_class (String Players)
+	{
+		final JPanel panel = new JPanel();
+		
+	
+
+}
+}
