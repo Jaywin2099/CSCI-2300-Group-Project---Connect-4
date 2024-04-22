43643cfb061a8ee0b14816682846ff3ae86e5bcf
Jeddrick Gamilla <jeddrick.gamilla@slu.edu>
2024-04-22 14:54:31 -0500

    alpha

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,45 @@
 
 public class Game {
+	int turnsPlayed = 0;
+	Board_class board;
+	
+	public Game(Board_class board) {	
+		this.board = board;
+	}
 
+	public boolean isWin() {
+		if(turnsPlayed > 7) {
+			
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void getCurrentPieceLocation(){
+		
+	}
+	
+	public void move(int column) {
+		if(getPlayerCheck() == 0) { //player 1
+			
+		}
+		turnsPlayed++;
+	}
+	
+	public int getCurrentTurn() {
+		return turnsPlayed;
+	}
+
+	public void restart() {
+		
+	}
+	
+	public int getPlayerCheck() {
+		return turnsPlayed % 2; // if odd turn, next turn is player 2, if even player 1
+	}
+	
 }
+
+
+
