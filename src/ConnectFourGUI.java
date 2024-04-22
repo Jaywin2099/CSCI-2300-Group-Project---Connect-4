600cbeb82258abfc3ecb65a803e3659cfbc12ab3
Josh Budzynski <josh.budzynski@slu.edu>
2024-04-22 15:13:02 -0500

    renamed board_class

---
+++ src/ConnectFourGUI.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Board_class extends JFrame{
+	private Game c4;
+
+	// jacob: im passing the model to the board, so when the player presses a column just use c4.[whatever jeddrick decides the method for handling moves]( the column);
+	
+	public Board_class (Game connect4)
+	{
+		this.c4 = connect4;
+
+		final JPanel panel = new JPanel();
+		
+	
+
+}
+}
