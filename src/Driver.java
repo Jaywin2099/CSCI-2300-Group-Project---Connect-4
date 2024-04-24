d91ba5ac179c8b2cf019b07ea7ebebe35ef76de1
Jacob Winter <jaywin2099@gmail.com>
2024-04-24 14:47:02 -0500

    driver changes

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,8 @@
 public class Driver {
     public static void main(String[] args) {
         // expects the number of players passed as argument
-        if (args.length != 1) {
-            System.out.println("Pass Number of Players as Argument: '1' or '2' ");
+        if (args.length != 1 && (!args[0].equals("1") || !args[0].equals("2"))) {
+            System.out.println("Pass Number of Players as Argument: 1 or 2 ");
             System.exit(0);
         }
 
