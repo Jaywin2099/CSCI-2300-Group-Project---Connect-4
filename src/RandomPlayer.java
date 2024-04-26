07b4fbc7722aba2b7c96c8a628ff0e31636ca402
Jacob Winter <jaywin2099@gmail.com>
2024-04-26 14:12:51 -0500

    blank

--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -3,11 +3,7 @@ import java.util.Random;
 public class RandomPlayer implements Observer {
     private Game connect4;
 
-<<<<<<< HEAD:src/ComputerPlayer.java
-    public ComputerPlayer (Game game) {
-=======
     public RandomPlayer (Game game) {
->>>>>>> 51b2c82d2d81117b1aa9b82c78d16dca5dea97f6:src/RandomPlayer.java
         this.connect4 = game;
     }
 
